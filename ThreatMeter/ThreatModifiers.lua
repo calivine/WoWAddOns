e97ced49a412308e69b2ac45c7842147001eefe7
Alex Caloggero <a.calog@protonmail.com>
2019-10-28 17:13:19 -0400

    Added special abilities' threat generation

--- ThreatMeter/ThreatModifiers.lua
+++ ThreatMeter/ThreatModifiers.lua
@@ -34,7 +34,41 @@ SPECIAL_ABILITIES = {
             21,
             32,
             43
-        }
+        },
+        CLEAVE = {
+            10,
+            40,
+            60,
+            70,
+            100
+        },
+        HAMSTRING = {
+            61,
+            101,
+            141
+        },
+        REVENGE = {
+            155,
+            195,
+            235,
+            275,
+            315,
+            355
+        },
+        SUNDER_ARMOR = {
+            100,
+            140,
+            180,
+            220,
+            260
+        },
+        SHIELD_SLAM = {
+            160,
+            190,
+            220,
+            250
+        },
+        SHIELD_BASH = 180
     },
     MAGE = {
         COUNTERSPELL = {
@@ -42,6 +76,86 @@ SPECIAL_ABILITIES = {
         },
         REMOVE_LESSER_CURSE = {
             14
+        }
+    },
+    DRUID = {
+        COWER = {
+            -240,
+            -390,
+            -600
+        },
+        DEMORALIZING_ROAR = {
+            9,
+            15,
+            20,
+            30,
+            39
+        },
+        FAERIE_FIRE = 108;
+    },
+    HUNTER = {
+        DISTRACTING_SHOT = {
+            100,
+            200,
+            300,
+            400,
+            500,
+            600
+        }
+    },
+    PET = {
+        COWER = {
+            -30,
+            -55,
+            -85,
+            -125,
+            -175,
+            -225
+        },
+        SCORPID_POISON = 5,
+        SUFFERING = {
+            200,
+            300,
+            450,
+            600
+        }
+    },
+    PALADIN = {
+        HOLY_SHIELD = {
+            20,
+            30,
+            40
+        },
+        CLEANSE = 40
+    },
+    PRIEST = {
+        FADE = {
+            -55,
+            -155,
+            -285,
+            -440,
+            -620,
+            -820
+        }
+    },
+    ROGUE = {
+        FEINT = {
+            -150,
+            -240,
+            -390,
+            -600,
+            -800
+        }
+    },
+    SHAMAN = {
+        ROCKBITER_WEAPON = {
+            6,
+            10,
+            16,
+            27,
+            41,
+            55,
+            72
         }
     }
 }
