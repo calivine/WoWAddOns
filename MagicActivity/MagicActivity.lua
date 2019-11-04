b82ce7168313536cc04549bfaa5fb8f83a8162cb
Alex Caloggero <a.calog@protonmail.com>
2019-11-04 13:03:46 -0500

    Minor fixes/updates

--- MagicActivity/MagicActivity.lua
+++ MagicActivity/MagicActivity.lua
@@ -1,3 +1,5 @@
+local SESSION_KILL_COUNT = 0;
+
 local MagicActivity = CreateFrame("Frame", "MagicActivity", UIParent);
 
 MagicActivity:SetFrameStrata("LOW");
@@ -5,17 +7,40 @@ MagicActivity:SetPoint("RIGHT");
 MagicActivity:SetWidth("175");
 MagicActivity:SetHeight("100");
 
-function MagicActivity_OnLoad(self)
-    self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-    SESSION_KILL_COUNT = 0;
-    print(SESSION_KILL_COUNT);
-end
 
-MagicActivity:SetScript("OnLoad", MagicActivity_OnLoad);
 
 function MagicActivity:OnEvent(event, ...)
+    if ( event == "COMBAT_LOG_EVENT_UNFILTERED" ) then 
+        local timestamp, combatEvent, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, args = CombatLogGetCurrentEventInfo();
+        
+        -- If combatEvent is not about the party, return. 
+        if ( bit.band(sourceFlags, COMBATLOG_OBJECT_AFFILIATION_MASK) > COMBATLOG_OBJECT_AFFILIATION_PARTY ) then
+            return;
+        end
+
+
+
+    elseif ( event == "PLAYER_LOGIN" ) then
+        self:Initialize();
+    end
 
 end
 
 MagicActivity:SetScript("OnEvent", MagicActivity.OnEvent)
 
+if ( IsLoggedIn() ) then
+    MagicActivity:Initialize();
+else
+    MagicActivity:RegisterEvent("PLAYER_LOGIN");
+end
+
+function MagicActivity:Initialize()
+    self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+end
+
+function MagicActivity:ProcessEntry(timestamp, combatEvent, hideCaster, srcGUID, srcName, srcFlags, srcRaidFlags, destGUID, destName, destFlags, destRaidFlags, ...)
+    if ( combatEvent == "UNIT_DIED" ) then
+        SESSION_KILL_COUNT = SESSION_KILL_COUNT + 1;
+        -- Call updateFrame method to change display
+    end
+end
