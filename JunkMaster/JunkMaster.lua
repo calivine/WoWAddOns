a9afdb6dcbb46fef5ddaddee7fcb5b521b0c9cec
Alex Caloggero <a.calog@protonmail.com>
2020-04-10 14:31:45 -0400

    Added bagaudit slash command

--- JunkMaster/JunkMaster.lua
+++ JunkMaster/JunkMaster.lua
@@ -1,11 +1,26 @@
+SLASH_BAGAUDIT1 = "/bagaudit";
+
+SlashCmdList["BAGAUDIT"] = function()
+    for bag=0,4 do
+        local bagSlots = GetContainerNumSlots(bag);
+        print(GetBagName(bag));
+        print(bagSlots);
+        for slot=1,bagSlots do
+            local icon, itemCount, locked, quality, readable, lootable, itemLink, isFiltered, noValue, itemID = GetContainerItemInfo(bag, slot);
+            if ( itemLink ~= nil ) then
+                print(itemLink, self:ItemQuality(quality));
+            end
+        end
+    end
+end
+
 local JM = CreateFrame("Frame", "JM", UIParent);
 
 function JM:OnEvent(event, ...)
     if ( event == "PLAYER_LOGIN" ) then
         self:Initialize();
-
     elseif ( event == "MERCHANT_SHOW" ) then
-        self:BagReview();
+        self:SellJunk();
     end
 end
 
@@ -21,19 +36,15 @@ function JM:Initialize()
     self:RegisterEvent("MERCHANT_SHOW")
 end
 
-
-function JM:BagReview()
+-- Cycle through all bags and slots and sells poor quality items
+-- Happens when players interacts with Merchant.
+function JM:SellJunk()
     for i=0,4 do
-        print(GetBagName(i));
-        print(GetContainerNumSlots(i));
         local bagSlots = GetContainerNumSlots(i);
         for j=1,bagSlots do
             local icon, itemCount, locked, quality, readable, lootable, itemLink, isFiltered, noValue, itemID = GetContainerItemInfo(i, j);
-            if ( itemLink ~= nil ) then
-                if ( quality == 0 ) then
-                    UseContainerItem(i, j);
-                end
-                print(itemLink, self:ItemQuality(quality));
+            if ( itemLink ~= nil and quality == 0 ) then
+                UseContainerItem(i, j);
             end
         end
     end
